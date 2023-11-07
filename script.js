2203ebc6b349f4f47d6d01ed464f72fff32400ac
tarasend <tz.taras@gmail.com>
2023-11-07 20:54:20 +0200

    work with images and sidebar

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let nameout = document.querySelector('.name-info');
 
 let llcname = document.querySelector('.company-info');
 
-for (key in logo) {
-    let img = document.createElement('img');      //создается изображение (element)
-    img.setAttribute('data-key', key);         // добавляется дата атрибут
+for (kluch in logo) {
+    let img = document.createElement('img');          //создается изображение (element)
+    img.setAttribute('data-key', kluch);                // добавляется дата атрибут
     img.classList.add('logo-decor');
-    img.src = './pictures/' + key + '.png';          // прописываем путь (в сурсе указывается папка)
-    imageout.append(img);                           //вставляем внутрь блока
+    img.src = './pictures/' + kluch + '.png';           // прописываем путь (в сурсе указывается папка)
+    imageout.append(img);                             //вставляем внутрь блока
 }
 
 
@@ -20,9 +20,15 @@ function showinfo(event) {
     console.dir(event.target);
     let key = event.target.dataset['key'];
     console.log(key);
+
+    
     if (key === undefined) {
         return true;
     }
+    //по клику в коробках сначала снимается класс актив.  
+    document.querySelectorAll('.imgbox img').forEach(item => item.classList.remove('active'));
+    // после этого по тому же клику добавляется класс актив
+    event.target.classList.add('active');
     nameout.textContent = logo[key]['name'];
     llcname.textContent = logo[key]['company'];
 
